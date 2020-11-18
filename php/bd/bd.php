fdf97bb8f183beac6042735cc3220737ca396672
Ershova <anna.yershova@hneu.net>
2020-11-18 23:14:53 +0200

    Anna

---
+++ php/bd/bd.php
@@ -0,0 +1,8 @@
+<?php
+$db = mysqli_connect("localhost","root","", "mysql");
+//   $db = new mysqli("localhost","root","");
+//	mysqli_connect("localhost","root","", "account_base");
+//  mysqli_query ("account_base",$db);
+//	mysqli_connect("localhost","root","", "my_db");
+// new mysqli("localhost", "my_user", "my_password", "test");
+?>
---
+++ php/data/data_contact.php
@@ -0,0 +1,6 @@
+<?php
+// Данные модуля Контактная_информация
+$phone = '+38 (073) 81-60-081';
+$mail = 'rock-zrada@gmail.com';
+$instagram = '@gadgik_shop';
+?>
